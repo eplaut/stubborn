a9dca225a062fe075834a0b67382a0ec8292f62e
Eli Plaut <eplaut@gmail.com>
2023-04-13 20:24:39 +0300

    register method

--- stubborn/app.py
+++ stubborn/app.py
@@ -1,13 +1,18 @@
 from typing import Dict
 
-from litestar import Litestar, HttpMethod
-from litestar.handlers.http_handlers.decorators import HTTPRouteHandler
+from litestar import Litestar, HttpMethod, Controller, post, route
 
 
-@HTTPRouteHandler("/", http_method=list(HttpMethod))
-def hello_world() -> Dict[str, str]:
-    """Keeping the tradition alive with hello world."""
-    return {"hello": "world"}
+class Stubborn(Controller):
+    @post("/_register")
+    def register(self) -> Dict[str, str]:
+        """Keeping the tradition alive with hello world."""
+        return {"hello": "world"}
+
+    @route("/", http_method=list(HttpMethod))
+    def replay(self) -> Dict[str, str]:
+        """Keeping the tradition alive with hello world."""
+        return {"hello": "world"}
 
 
-app = Litestar(route_handlers=[hello_world])
+app = Litestar(route_handlers=[Stubborn])
