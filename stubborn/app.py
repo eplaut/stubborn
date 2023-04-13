97b706b5b06c906c16c6233b55fb4b69fe524ace
Eli Plaut <eplaut@gmail.com>
2023-04-13 20:24:39 +0300

    store requests in state

--- stubborn/app.py
+++ stubborn/app.py
@@ -1,18 +1,22 @@
 from typing import Dict
 
-from litestar import Litestar, HttpMethod, Controller, post, route
+from litestar import Litestar, Request, HttpMethod, Controller, post, route
+from litestar.datastructures import State
 
 
 class Stubborn(Controller):
     @post("/_register")
-    def register(self) -> Dict[str, str]:
-        """Keeping the tradition alive with hello world."""
-        return {"hello": "world"}
+    async def register(self, state: State, request: Request) -> Dict[str, str]:
+        res = await request.json()
+        if res["path"] not in state:
+            state[res["path"]] = {}
+        state[res["path"]][res["method"]] = (res.get("count", float("inf")), res["response"])
+        return {"result": "success"}
 
     @route("/", http_method=list(HttpMethod))
-    def replay(self) -> Dict[str, str]:
+    def replay(self, state: State) -> Dict[str, str]:
         """Keeping the tradition alive with hello world."""
-        return {"hello": "world"}
+        return dict(state)
 
 
 app = Litestar(route_handlers=[Stubborn])
