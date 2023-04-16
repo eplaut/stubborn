b23fdfa55eca44bebf0bf944c0d4ee9e82749580
Eli Plaut <eplaut@gmail.com>
2023-04-16 18:36:07 +0300

    add more tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -45,3 +45,61 @@ def test_register_without_count():
             res = client.get(path)
             assert res.status_code == 200
             assert res.json() == expected_response_1
+
+
+def test_register_with_status_code():
+    with create_test_client([Stubborn]) as client:
+        expected_response_1 = {"result": "success"}
+        path = "/test"
+        status_code = 201
+        client.post(
+            "/_register",
+            json={
+                "path": path,
+                "method": "GET",
+                "count": 1,
+                "response": expected_response_1,
+                "status_code": status_code,
+            },
+        )
+        res = client.get(path)
+        assert res.status_code == status_code
+        assert res.json() == expected_response_1
+
+
+def test_register_with_different_methods():
+    with create_test_client([Stubborn]) as client:
+        expected_response_1 = {"result": "success"}
+        path = "/test"
+        client.post(
+            "/_register",
+            json={
+                "path": path,
+                "method": "POST",
+                "count": 1,
+                "response": expected_response_1,
+            },
+        )
+        res = client.get(path)
+        assert res.status_code == 404
+        res = client.post(path)
+        assert res.status_code == 200
+        assert res.json() == expected_response_1
+
+
+def test_register_multiple_paths():
+    with create_test_client([Stubborn]) as client:
+        expected_response_1 = {"result": "success"}
+        expected_response_2 = {"result": "success2"}
+        path_1 = "/test"
+        path_2 = "/test2"
+        client.post("/_register", json={"path": path_1, "method": "GET", "count": 1, "response": expected_response_1})
+        client.post("/_register", json={"path": path_2, "method": "GET", "count": 1, "response": expected_response_2})
+        res = client.get(path_1)
+        assert res.status_code == 200
+        assert res.json() == expected_response_1
+        res = client.get(path_1)
+        assert res.status_code == 404
+        res = client.get(path_2)
+        assert res.status_code == 200
+        assert res.json() == expected_response_2
